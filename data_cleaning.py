134817101f02ec79bf68d4772745822f9390cd3b
LucjaWuls <lucjawuls@student.agh.edu.pl>
2025-04-03 13:21:59 +0200

    Poprawki, usunięcie danych teksotwych lub ich zamiana

--- data_cleaning.py
+++ data_cleaning.py
@@ -13,19 +13,33 @@ test_df['Fare'] = test_df['Fare'].fillna(test_df['Fare'].median())
 train_df['Age'].fillna(train_df['Age'].median(), inplace=True)
 test_df['Age'].fillna(test_df['Age'].median(), inplace=True)
 
-# Uzupełnienie braków w 'Embarked' najczęściej występującą wartością (trybem)
+# Uzupełnienie braków w 'Embarked' najczęściej występującą wartością i zamiana na liczby
 most_common_embarked = train_df['Embarked'].mode()[0]
 train_df['Embarked'] = train_df['Embarked'].fillna(most_common_embarked)
-
-
 if test_df['Embarked'].isnull().sum() > 0:
     test_df['Embarked'].fillna(most_common_embarked, inplace=True)
+embarked_mapping = {'S': 0, 'C': 1, 'Q': 2}
+train_df['Embarked'] = train_df['Embarked'].map(embarked_mapping)
+test_df['Embarked'] = test_df['Embarked'].map(embarked_mapping)
 
-# Usunięcie kolumny 'Cabin'
+
+#Zamiana female/male na 0/1
+train_df['Sex'] = train_df['Sex'].map({'female': 0, 'male': 1})
+test_df['Sex'] = test_df['Sex'].map({'female': 0, 'male': 1})
+
+# Usunięcie kolumn "Cabin", "Name", "Ticket"
 if 'Cabin' in train_df.columns:
     train_df.drop('Cabin', axis=1, inplace=True)
 if 'Cabin' in test_df.columns:
     test_df.drop('Cabin', axis=1, inplace=True)
+if 'Name' in train_df.columns:
+    train_df.drop('Name', axis=1, inplace=True)
+if 'Name' in test_df.columns:
+    test_df.drop('Name', axis=1, inplace=True)
+if 'Ticket' in train_df.columns:
+    train_df.drop('Ticket', axis=1, inplace=True)
+if 'Ticket' in test_df.columns:
+    test_df.drop('Ticket', axis=1, inplace=True)
 
 # Krótka weryfikacja braków
 print("\n=== Braki danych w Train (po uzupełnieniu) ===")
